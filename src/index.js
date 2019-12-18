39ab391aec0c3d6e257d38acb1594410f5fd38fd
David Darx <daviddarx@gmail.com>
2019-12-18 23:03:28 +0100

    feat(navigation) finalizes basic navigation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+import { EWOULDBLOCK } from "constants";
+
 //pictures
 const Picture = function () {
 
   this.settings = {
+    id: 0,
     windowWMax: 0,
     windowHMax: 0,
     imgWidth: 0,
@@ -22,7 +25,8 @@ const Picture = function () {
 
   this.isLoaded = false;
 
-  this.init = (picture) => {
+  this.init = (picture, id) => {
+    this.settings.id = id;
     this.refs.$div = picture;
     this.refs.$img = picture.querySelector('img');
   };
@@ -38,19 +42,25 @@ const Picture = function () {
     this.refs.$img.parentNode.replaceChild($imageClone, this.refs.$img);
     this.refs.$img = $imageClone;
 
-    this.settings.imgWidth = this.refs.$img.naturalWidth;
-    this.settings.imgHeight = this.refs.$img.naturalHeight;
+    setTimeout(() => { //safari debug naturalWidth
+      this.settings.imgWidth = this.refs.$img.naturalWidth;
+      this.settings.imgHeight = this.refs.$img.naturalHeight;
 
-    this.settings.widthRatioToHeight = this.settings.imgWidth / this.settings.imgHeight;
-    this.settings.heightRatioToWidth = this.settings.imgHeight / this.settings.imgWidth;
+      console.log(this.settings.id);
+      console.log(this.settings.imgWidth + " " + this.settings.imgHeight);
+      console.log("----");
 
-    this.refs.loadCompleteCallback();
-    this.refs.loadCompleteCallback = undefined;
+      this.settings.widthRatioToHeight = this.settings.imgWidth / this.settings.imgHeight;
+      this.settings.heightRatioToWidth = this.settings.imgHeight / this.settings.imgWidth;
 
-    this.refs.$div.classList.add('is-loaded');
+      this.refs.loadCompleteCallback();
+      this.refs.loadCompleteCallback = undefined;
 
-    this.isLoaded = true;
-    this.resize();
+      this.isLoaded = true;
+      this.resize();
+
+      this.refs.$div.classList.add('is-loaded');
+    }, 100);
   };
 
   this.setWindowSizes = (winW, winH) => {
@@ -63,53 +73,114 @@ const Picture = function () {
       this.settings.width = this.settings.windowWMax * this.settings.widthRatioToWindow;
       this.settings.height = this.settings.width * this.settings.heightRatioToWidth;
 
+      //console.log(this.settings.heightRatioToWidth);
+
       let maxHeight = this.settings.windowHMax * this.settings.heightRatioToWindow;
 
       if (this.settings.height > maxHeight) {
         this.settings.height = maxHeight;
-        this.settings.width = this.settings.height* this.settings.widthRatioToHeight;
+        this.settings.width = this.settings.height * this.settings.widthRatioToHeight;
       }
 
       this.refs.$div.style.width = this.settings.width + 'px';
       this.refs.$div.style.height = this.settings.height + 'px';
+
+      // console.log(this.settings.id);
+      // console.log(maxHeight + " " + this.settings.height + " ");
+      // console.log("----");
     }
   };
 };
 
 const Pictures = function () {
   this.settings = {
-    maxWindowWidth: 1440
+    maxWindowWidth: 1440,
+    loadCurrentID: 0,
+    totalPictures: 0,
+    currentID: 0
   };
 
   this.refs = {
     $picturesContainer: undefined,
     $pictures: undefined,
     picturesRep: undefined,
+    $navPrev: undefined,
+    $navNext: undefined
   };
-
-  this.currentID = 0;
 
   this.init = () => {
     this.refs.$picturesContainer = document.querySelector('.pictures');
     this.refs.$pictures = document.querySelectorAll('.picture');
     this.refs.picturesRep = [];
 
-    this.refs.$pictures.forEach((picture) => {
+    this.refs.$navPrev = document.querySelector('.navigation-button--prev');
+    this.refs.$navNext = document.querySelector('.navigation-button--next');
+
+    this.refs.$navPrev.addEventListener('click', this.navigateButton);
+    this.refs.$navPrev.addEventListener('touchstart', this.navigateButton);
+    this.refs.$navNext.addEventListener('click', this.navigateButton);
+    this.refs.$navNext.addEventListener('touchstart', this.navigateButton);
+
+    window.addEventListener('keydown', this.navigationKeyboard, false);
+
+    this.refs.$pictures.forEach((picture, i) => {
       const pictureItem = new Picture();
-            pictureItem.init(picture);
+            pictureItem.init(picture, i);
       this.refs.picturesRep.push(pictureItem);
     });
 
-    this.refs.picturesRep[this.currentID].load(this.loadComplete);
+    this.settings.totalPictures = this.refs.picturesRep.length;
+
+    this.refs.picturesRep[this.settings.loadCurrentID].load(this.loadComplete);
   };
 
   this.loadComplete = () => {
-    this.currentID ++;
+    this.settings.loadCurrentID ++;
 
-    if(this.currentID < this.refs.picturesRep.length) {
-      this.refs.picturesRep[this.currentID].load(this.loadComplete);
+    if(this.settings.loadCurrentID < this.refs.picturesRep.length) {
+      this.refs.picturesRep[this.settings.loadCurrentID].load(this.loadComplete);
     }
   };
+
+  this.scrollToPicture = () => {
+    this.refs.$pictures[this.settings.currentID].scrollIntoView({ behavior: 'smooth'});
+  }
+
+  this.setPicturePrev = () => {
+    if (this.settings.currentID > 0 ) {
+      this.settings.currentID--;
+    }
+    this.scrollToPicture();
+  }
+
+  this.setPictureNext = () => {
+    if (this.settings.currentID < this.settings.totalPictures - 1 ) {
+      this.settings.currentID++;
+    }
+    this.scrollToPicture();
+  }
+
+  this.navigateButton = (e) => {
+    if (e.currentTarget == this.refs.$navPrev) {
+      this.setPicturePrev();
+    } else {
+      this.setPictureNext();
+    }
+  };
+
+  this.navigationKeyboard = (e) => {
+    const key = e.keyCode ? e.keyCode : e.which;
+
+    if (key == 38) {
+      this.setPicturePrev();
+    } else if (key == 40) {
+      this.setPictureNext();
+    }
+
+    if (key == 38 || key == 40) {
+      e.preventDefault();
+    }
+  }
 
   this.resize = () => {
     const finalWindowW = (windowW < this.settings.maxWindowWidth) ? windowW : this.settings.maxWindowWidth;
@@ -141,24 +212,6 @@ const toggleLightmode = () => {
 $lightmodeButton.addEventListener('click', toggleLightmode);
 $lightmodeButton.addEventListener('touchstart', toggleLightmode);
 
-
-
-//navigation
-const $navigationPrev = document.querySelector('.navigation-button--prev');
-const $navigationNext = document.querySelector('.navigation-button--next');
-
-const navigate = (e) => {
-  if (e.currentTarget == $navigationPrev) {
-    console.log("prev");
-  } else {
-    console.log("next");
-  }
-};
-
-$navigationPrev.addEventListener('click', navigate);
-$navigationPrev.addEventListener('touchstart', navigate);
-$navigationNext.addEventListener('click', navigate);
-$navigationNext.addEventListener('touchstart', navigate);
 
 
 //general
