bf1435f992abd5a40e3c09027ec19e6bd15ce78d
David Darx <daviddarx@gmail.com>
2020-03-16 20:43:42 +0100

    feat(structure) clears unuseful code for pixi

--- src/index.js
+++ src/index.js
@@ -1,98 +1,102 @@
 import * as PIXI from 'pixi.js'
+import * as content from '../content/pictures.json'
+
+delete content.default;
+const contentArray = Object.values(content);
 
 //pictures
-const Picture = function () {
+// const Picture = function () {
 
-  this.settings = {
-    id: 0,
-    thumbURL: '',
-    imgURL: '',
-    imgWidth: 0,
-    imgHeight: 0,
-    width: 0,
-    height: 0,
-    posX: 0,
-    posY: 0,
-    widthRatioToHeight: 0,
-    heightRatioToWidth: 0,
-    windowPadding: 0,
-    windowWMax: 0,
-    windowHMax: 0,
-  };
+//   this.settings = {
+//     id: 0,
+//     thumbURL: '',
+//     imgURL: '',
+//     imgWidth: 0,
+//     imgHeight: 0,
+//     width: 0,
+//     height: 0,
+//     posX: 0,
+//     posY: 0,
+//     widthRatioToHeight: 0,
+//     heightRatioToWidth: 0,
+//     windowPadding: 0,
+//     windowWMax: 0,
+//     windowHMax: 0,
+//   };
 
-  this.refs = {
-    $div: undefined,
-    $img: undefined,
-    loadCompleteCallback: undefined
-  };
+//   this.refs = {
+//     $div: undefined,
+//     $img: undefined,
+//     loadCompleteCallback: undefined
+//   };
 
-  this.isLoaded = false;
+//   this.isLoaded = false;
 
-  this.init = (picture, id, hd) => {
-    this.settings.id = id;
-    this.refs.$div = picture;
-    this.refs.$img = picture.querySelector('img');
+//   this.init = (picture, id, hd) => {
+//     this.settings.id = id;
+//     this.refs.$div = picture;
+//     this.refs.$img = picture.querySelector('img');
 
-    const splittedURL = this.refs.$img.getAttribute('rel').split('/');
-    this.settings.imgURL = splittedURL[0] + '/' + hd + splittedURL[1];
-    this.settings.thumbURL = splittedURL[0] + '/' +  '_thumbs/' + splittedURL[1];
-  };
+//     const splittedURL = this.refs.$img.getAttribute('rel').split('/');
+//     this.settings.imgURL = splittedURL[0] + '/' + hd + splittedURL[1];
+//     this.settings.thumbURL = splittedURL[0] + '/' +  '_thumbs/' + splittedURL[1];
+//   };
 
-  this.load = (loadCompleteCallback) => {
-    this.refs.$img.setAttribute('src', this.settings.imgURL);
-    this.refs.$img.addEventListener('load', this.loadComplete);
-    this.refs.loadCompleteCallback = loadCompleteCallback;
-  };
+//   this.load = (loadCompleteCallback) => {
+//     this.refs.$img.setAttribute('src', this.settings.imgURL);
+//     this.refs.$img.addEventListener('load', this.loadComplete);
+//     this.refs.loadCompleteCallback = loadCompleteCallback;
+//   };
 
-  this.loadComplete = () => {
-    const $imageClone = this.refs.$img.cloneNode(true);
-    this.refs.$img.parentNode.replaceChild($imageClone, this.refs.$img);
-    this.refs.$img = $imageClone;
+//   this.loadComplete = () => {
+//     const $imageClone = this.refs.$img.cloneNode(true);
+//     this.refs.$img.parentNode.replaceChild($imageClone, this.refs.$img);
+//     this.refs.$img = $imageClone;
 
-    setTimeout(() => { //safari debug naturalWidth
-      this.settings.imgWidth = this.refs.$img.naturalWidth;
-      this.settings.imgHeight = this.refs.$img.naturalHeight;
+//     setTimeout(() => { //safari debug naturalWidth
+//       this.settings.imgWidth = this.refs.$img.naturalWidth;
+//       this.settings.imgHeight = this.refs.$img.naturalHeight;
 
-      this.settings.widthRatioToHeight = this.settings.imgWidth / this.settings.imgHeight;
-      this.settings.heightRatioToWidth = this.settings.imgHeight / this.settings.imgWidth;
+//       this.settings.widthRatioToHeight = this.settings.imgWidth / this.settings.imgHeight;
+//       this.settings.heightRatioToWidth = this.settings.imgHeight / this.settings.imgWidth;
 
-      this.refs.loadCompleteCallback();
-      this.refs.loadCompleteCallback = undefined;
+//       this.refs.loadCompleteCallback();
+//       this.refs.loadCompleteCallback = undefined;
 
-      this.isLoaded = true;
-      this.resize();
+//       this.isLoaded = true;
+//       this.resize();
 
-      this.refs.$div.classList.add('is-loaded');
-    }, 100);
-  };
+//       this.refs.$div.classList.add('is-loaded');
+//     }, 100);
+//   };
 
-  this.setWindowPadding = (padding) => {
-    this.settings.windowPadding = padding;
-    this.settings.windowWMax = Math.floor(windowW - 2 * padding);
-    this.settings.windowHMax = Math.floor(windowH - 2 * padding);
-  }
+//   this.setWindowPadding = (padding) => {
+//     this.settings.windowPadding = padding;
+//     this.settings.windowWMax = Math.floor(windowW - 2 * padding);
+//     this.settings.windowHMax = Math.floor(windowH - 2 * padding);
+//   }
 
-  this.resize = () => {
-    if (this.isLoaded == true) {
-      this.settings.height = this.settings.windowHMax;
-      this.settings.width = this.settings.height *  this.settings.widthRatioToHeight;
-      this.settings.posX = Math.floor( this.settings.windowPadding + Math.random() * (this.settings.windowWMax - this.settings.width));
-      this.settings.posY = this.settings.windowPadding;
+//   this.resize = () => {
+//     if (this.isLoaded == true) {
+//       this.settings.height = this.settings.windowHMax;
+//       this.settings.width = this.settings.height *  this.settings.widthRatioToHeight;
+//       this.settings.posX = Math.floor( this.settings.windowPadding + Math.random() * (this.settings.windowWMax - this.settings.width));
+//       this.settings.posY = this.settings.windowPadding;
 
-      if (this.settings.width > this.settings.windowWMax) {
-        this.settings.width = this.settings.windowWMax;
-        this.settings.height = this.settings.width * this.settings.heightRatioToWidth;
-        this.settings.posX = this.settings.windowPadding;
-        this.settings.posY = Math.floor( this.settings.windowPadding + Math.random() * (this.settings.windowHMax - this.settings.height));
-      }
+//       if (this.settings.width > this.settings.windowWMax) {
+//         this.settings.width = this.settings.windowWMax;
+//         this.settings.height = this.settings.width * this.settings.heightRatioToWidth;
+//         this.settings.posX = this.settings.windowPadding;
+//         this.settings.posY = Math.floor( this.settings.windowPadding + Math.random() * (this.settings.windowHMax - this.settings.height));
+//       }
 
-      this.refs.$div.style.width = this.settings.width + 'px';
-      this.refs.$div.style.height = this.settings.height + 'px';
-      this.refs.$div.style.left = this.settings.posX + 'px';
-      this.refs.$div.style.top = this.settings.posY + 'px';
-    }
-  };
-};
+//       this.refs.$div.style.width = this.settings.width + 'px';
+//       this.refs.$div.style.height = this.settings.height + 'px';
+//       this.refs.$div.style.left = this.settings.posX + 'px';
+//       this.refs.$div.style.top = this.settings.posY + 'px';
+//     }
+//   };
+// };
 
 
 
@@ -110,31 +114,25 @@ const App = function () {
   };
 
   this.refs = {
-    $picturesContainer: undefined,
-    $pictures: undefined,
-    picturesRep: undefined,
-    $thumbsContainer: undefined,
-    thumbsApp: undefined,
+    app: undefined,
     thumbsRep: undefined,
+    picturesRep: undefined,
     $nav: undefined,
     $navPrev: undefined,
     $navNext: undefined,
     $lightmodeButton: undefined,
     $paginationCurrent: undefined,
     $paginationTotal: undefined,
-    timeoutChangeLightmode: undefined,
-    timeoutDisplayNextPicture: undefined,
-    timeoutHidePreviousPicture: undefined,
-    timeoutWheelDebounce: undefined,
-    pictureToHide: undefined,
+    // timeoutChangeLightmode: undefined,
+    // timeoutDisplayNextPicture: undefined,
+    // timeoutHidePreviousPicture: undefined,
+    // timeoutWheelDebounce: undefined,
+    // pictureToHide: undefined,
   };
 
   this.init = () => {
-    this.refs.$picturesContainer = document.querySelector('.pictures');
-    this.refs.$pictures = document.querySelectorAll('.picture');
+    this.refs.$app = document.querySelector('.app');
     this.refs.picturesRep = [];
-
-    this.refs.$thumbsContainer = document.querySelector('.thumbs');
     this.refs.thumbsRep = [];
 
     this.refs.$nav = document.querySelector('.navigation');
@@ -144,54 +142,52 @@ const App = function () {
     this.refs.$navPrev.addEventListener('touchstart', this.navigateButton);
     this.refs.$navNext.addEventListener('click', this.navigateButton);
     this.refs.$navNext.addEventListener('touchstart', this.navigateButton);
-    window.addEventListener('keydown', this.navigateKeyboard, false);
 
     this.refs.$lightmodeButton = document.querySelector('.lightmode-button');
     this.refs.$lightmodeButton.addEventListener('click', this.toggleLightmode);
     this.refs.$lightmodeButton.addEventListener('touchstart', this.toggleLightmode);
 
+
     const imgHDFolder = (windowW * window.devicePixelRatio > 1600) ? '_hd/' : '';
 
-    this.refs.$pictures.forEach((picture, i) => {
-      picture.setAttribute('rel', i);
-      const pictureItem = new Picture();
-            pictureItem.init(picture, i, imgHDFolder);
-      this.refs.picturesRep.push(pictureItem);
+    contentArray.forEach((item) => {
+      // const pictureItem = new Picture();
+      //       pictureItem.init(picture, i, imgHDFolder);
+      // this.refs.picturesRep.push(pictureItem);
     });
 
-    this.settings.totalPictures = this.refs.picturesRep.length;
+
+    this.settings.totalPictures = 12; //this.refs.picturesRep.length;
 
     this.refs.$paginationCurrent = document.querySelector('.pagination__current');
     this.refs.$paginationTotal = document.querySelector('.pagination__total');
     this.refs.$paginationTotal.innerHTML = this.settings.totalPictures;
     this.updatePagination();
 
-    // this.refs.picturesRep[this.settings.loadCurrentID].load(this.loadComplete);
-    // this.changePicture();
-    // this.changeLightmode(parseInt(this.refs.$pictures[this.settings.currentID].getAttribute('data-bg')));
+    // window.addEventListener('keydown', this.navigateKeyboard, false);
+    // window.addEventListener('wheel', (e) => {
+    //   if (this.refs.timeoutWheelDebounce) {
+    //     clearTimeout(this.refs.timeoutWheelDebounce);
+    //   }
+    //   this.refs.timeoutWheelDebounce = setTimeout(()=> {
+    //     this.navigateWheel(e.deltaY);
+    //   }, 100);
+    // });
 
-    window.addEventListener('wheel', (e) => {
-      if (this.refs.timeoutWheelDebounce) {
-        clearTimeout(this.refs.timeoutWheelDebounce);
-      }
-      this.refs.timeoutWheelDebounce = setTimeout(()=> {
-        this.navigateWheel(e.deltaY);
-      }, 100);
-    });
-
-    this.setThumbs();
+    // this.setThumbs();
+    this.toggleLightmode();
   };
 
   this.setThumbs = () => {
-    this.refs.thumbsApp = new PIXI.Application({
+    this.refs.app = new PIXI.Application({
       backgroundColor:0xffffff,
       antialias: true,
       transparent: false,
       resolution: 1,
 
     });
-    this.refs.thumbsApp.renderer.autoResize = true;
-    this.refs.$thumbsContainer.appendChild(this.refs.thumbsApp.view);
+    this.refs.app.renderer.autoResize = true;
+    this.refs.$app.appendChild(this.refs.app.view);
 
     this.loadThumb();
   }
@@ -204,7 +200,7 @@ const App = function () {
 
   this.createThumb = () => {
     const thumb = new PIXI.Sprite(PIXI.Loader.shared.resources[this.refs.picturesRep[this.settings.loadThumbsCurrentID].settings.thumbURL].texture);
-    this.refs.thumbsApp.stage.addChild(thumb);
+    this.refs.app.stage.addChild(thumb);
     this.refs.thumbsRep.push(thumb);
     this.positionThumb(this.settings.loadThumbsCurrentID);
 
@@ -220,73 +216,74 @@ const App = function () {
   }
 
   this.resizeThumbs = () => {
-    this.refs.thumbsApp.renderer.resize(windowW, windowH);
+    // this.refs.app.renderer.resize(windowW, windowH);
   }
 
-  this.loadComplete = () => {
-    this.settings.loadCurrentID ++;
+  // this.loadComplete = () => {
+  //   this.settings.loadCurrentID ++;
 
-    if(this.settings.loadCurrentID < this.refs.picturesRep.length) {
-      this.refs.picturesRep[this.settings.loadCurrentID].load(this.loadComplete);
-    }
-  };
+  //   if(this.settings.loadCurrentID < this.refs.picturesRep.length) {
+  //     this.refs.picturesRep[this.settings.loadCurrentID].load(this.loadComplete);
+  //   }
+  // };
 
-  this.changePicture = () => {
-    this.updatePagination();
+  // this.changePicture = () => {
+  //   this.updatePagination();
 
-    this.refs.$navPrev.classList.remove('is-disabled');
-    this.refs.$navNext.classList.remove('is-disabled');
+  //   this.refs.$navPrev.classList.remove('is-disabled');
+  //   this.refs.$navNext.classList.remove('is-disabled');
 
-    if (this.settings.currentID == 0) {
-      this.refs.$navPrev.classList.add('is-disabled');
-    } else if (this.settings.currentID == this.settings.totalPictures - 1) {
-      this.refs.$navNext.classList.add('is-disabled');
-    }
+  //   if (this.settings.currentID == 0) {
+  //     this.refs.$navPrev.classList.add('is-disabled');
+  //   } else if (this.settings.currentID == this.settings.totalPictures - 1) {
+  //     this.refs.$navNext.classList.add('is-disabled');
+  //   }
 
-    this.refs.$pictures[this.settings.currentID].classList.add('is-on-top');
-    this.refs.$pictures[this.settings.currentID].classList.add('is-active');
+  //   this.refs.$pictures[this.settings.currentID].classList.add('is-on-top');
+  //   this.refs.$pictures[this.settings.currentID].classList.add('is-active');
 
-    this.changeLightmode(true);
+  //   this.changeLightmode(true);
 
-    if (this.refs.timeoutDisplayNextPicture != undefined) {
-      clearTimeout(this.refs.timeoutDisplayNextPicture);
-    }
-    this.refs.timeoutDisplayNextPicture = setTimeout(this.displayNextPicture, 100);
+  //   if (this.refs.timeoutDisplayNextPicture != undefined) {
+  //     clearTimeout(this.refs.timeoutDisplayNextPicture);
+  //   }
+  //   this.refs.timeoutDisplayNextPicture = setTimeout(this.displayNextPicture, 100);
 
-    if (this.settings.previousID != undefined) {
-      this.refs.$pictures[this.settings.previousID].classList.remove('is-on-top');
-      if (this.refs.timeoutHidePreviousPicture != undefined) {
-        clearTimeout(this.refs.timeoutHidePreviousPicture);
-        if (this.settings.currentID != parseInt(this.refs.pictureToHide.getAttribute("rel"))) { //to avoid overwriting bug if user go next/prev quickly
-          this.deactivePreviousPicture();
-        }
-      }
-      this.refs.pictureToHide = this.refs.$pictures[this.settings.previousID];
-      this.refs.timeoutHidePreviousPicture = setTimeout(this.hidePreviousPicture, 1000);
-    }
-  };
+  //   if (this.settings.previousID != undefined) {
+  //     this.refs.$pictures[this.settings.previousID].classList.remove('is-on-top');
+  //     if (this.refs.timeoutHidePreviousPicture != undefined) {
+  //       clearTimeout(this.refs.timeoutHidePreviousPicture);
+  //       if (this.settings.currentID != parseInt(this.refs.pictureToHide.getAttribute("rel"))) { //to avoid overwriting bug if user go next/prev quickly
+  //         this.deactivePreviousPicture();
+  //       }
+  //     }
+  //     this.refs.pictureToHide = this.refs.$pictures[this.settings.previousID];
+  //     this.refs.timeoutHidePreviousPicture = setTimeout(this.hidePreviousPicture, 1000);
+  //   }
+  // };
 
-  this.displayNextPicture = () => {
-    this.refs.$pictures[this.settings.currentID].classList.add('is-displayed');
-    this.refs.timeoutDisplayNextPicture = undefined;
-  };
+  // this.displayNextPicture = () => {
+  //   this.refs.$pictures[this.settings.currentID].classList.add('is-displayed');
+  //   this.refs.timeoutDisplayNextPicture = undefined;
+  // };
 
-  this.hidePreviousPicture = () => {
-    this.refs.pictureToHide.classList.remove('is-displayed');
-    this.refs.timeoutHidePreviousPicture = setTimeout(this.deactivePreviousPicture, 1700);
-  };
+  // this.hidePreviousPicture = () => {
+  //   this.refs.pictureToHide.classList.remove('is-displayed');
+  //   this.refs.timeoutHidePreviousPicture = setTimeout(this.deactivePreviousPicture, 1700);
+  // };
 
-  this.deactivePreviousPicture = () => {
-    this.refs.pictureToHide.classList.remove('is-displayed'); //for direct call to overwrite first animation out step
-    this.refs.pictureToHide.classList.remove('is-active');
-    this.refs.timeoutHidePreviousPicture = undefined;
-  };
+  // this.deactivePreviousPicture = () => {
+  //   this.refs.pictureToHide.classList.remove('is-displayed'); //for direct call to overwrite first animation out step
+  //   this.refs.pictureToHide.classList.remove('is-active');
+  //   this.refs.timeoutHidePreviousPicture = undefined;
+  // };
 
   this.setPicturePrev = () => {
     if (this.settings.currentID > 0 ) {
       this.settings.previousID = this.settings.currentID;
       this.settings.currentID--;
-      this.changePicture();
+      this.updatePagination();
+      // this.changePicture();
     }
   };
 
@@ -294,7 +291,8 @@ const App = function () {
     if (this.settings.currentID < this.settings.totalPictures - 1 ) {
       this.settings.previousID = this.settings.currentID;
       this.settings.currentID++;
-      this.changePicture();
+      this.updatePagination();
+      // this.changePicture();
     }
   };
 
@@ -321,16 +319,6 @@ const App = function () {
       e.preventDefault();
     }
   }
-
-  this.navigateWheel = (delta) => {
-    if (delta > 0) {
-      this.setPictureNext();
-    } else {
-      this.setPicturePrev();
-    }
-
-    return false;
-  };
 
   this.updatePagination = () => {
     let zero = '';
@@ -365,6 +353,16 @@ const App = function () {
     }, 1000);
   }
 
+  // this.navigateWheel = (delta) => {
+  //   if (delta > 0) {
+  //     this.setPictureNext();
+  //   } else {
+  //     this.setPicturePrev();
+  //   }
+
+  //   return false;
+  // };
+
   this.resize = () => {
     const padding = Math.floor(windowW * this.settings.windowPaddingRatioToW);
 
@@ -374,10 +372,10 @@ const App = function () {
     this.refs.$lightmodeButton.style.right = padding + 'px';
     this.refs.$lightmodeButton.style.top = padding + 'px';
 
-    this.refs.picturesRep.forEach((pictureItem) => {
-      pictureItem.setWindowPadding(padding);
-      pictureItem.resize();
-    });
+    // this.refs.picturesRep.forEach((pictureItem) => {
+    //   pictureItem.setWindowPadding(padding);
+    //   pictureItem.resize();
+    // });
 
     this.resizeThumbs();
   };
